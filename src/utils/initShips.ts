ea5edf930de46d67b793b4256efcf7eca8a44ce3
Liza <lizka4231@gmail.com>
2025-12-02 18:16:01 +0300

    feat(store): add ships state

---
+++ src/utils/initShips.ts
@@ -0,0 +1,29 @@
+import type { IShipItem, ShipType } from "../types/types"
+
+export const shipsConfig: Record<ShipType, { amount: number, size: number }> = {
+   'single': { amount: 4, size: 1 },
+   'duo': { amount: 3, size: 2 },
+   'trio': { amount: 2, size: 3 },
+   'quadro': { amount: 1, size: 4 }
+}
+
+export const initShips = (): IShipItem[] => {
+   const ships: IShipItem[] = []
+   let shipCounter = 1;
+
+   for (const shipType in shipsConfig) {
+      const { amount, size } = shipsConfig[shipType as ShipType];
+
+      for (let i: number = 0; i<amount; i++) {
+         ships.push({
+            id: String(shipCounter++),
+            state: 'ready',
+            size,
+            type: shipType as ShipType,
+            takenCellIds: []
+         })
+      }
+   }
+
+   return ships;
+}
