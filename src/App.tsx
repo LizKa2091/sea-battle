b3136b77638ef23ebc6479f4350152c9811130f9
lizka <lizka4231@gmail.com>
2025-12-06 21:21:41 +0300

    feat(App, main): add routes for MainPage, GamePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import { type FC } from "react";
-import Field from "./components/field/Field";
+import { Route, Routes, useLocation } from "react-router-dom";
+
+import MainPage from "./pages/MainPage";
+import GamePage from "./pages/GamePage";
 
 import './styles/reset.scss';
 
 const App: FC = () => {
+   const location = useLocation();
+   const state = location.state as { modal?: Location };
 
    return (
       <>
-         <Field />
+         <Routes>
+            <Route path='/' element={<MainPage />} />
+         </Routes>
+         {state?.modal && (
+            <Routes>
+               <Route path='/game' element={<GamePage />} />
+            </Routes>
+         )}
       </>
    )
 }
