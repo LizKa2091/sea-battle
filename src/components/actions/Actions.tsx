614f02f96a389e0b1fd241f2bbb4ffe05b4e81ad
lizka <lizka4231@gmail.com>
2025-12-09 23:05:32 +0300

    feat(actions): add enemy's turn imitation after player's turn

--- src/components/actions/Actions.tsx
+++ src/components/actions/Actions.tsx
@@ -1,10 +1,11 @@
-import { type FC } from 'react'
+import { useEffect, useState, type FC } from 'react'
 import ActionButton from '../action-button/ActionButton';
 import { useGameStore } from '../../store/useGameStore';
 import { useSelectedCells } from '../../hooks/useSelectedCells';
 import type { User } from '../../types/types';
 
 import styles from './Actions.module.scss';
+import { useDamageRandomCell } from '../../hooks/useDamageRandomCell';
 
 interface IUserActions {
    user: User;
@@ -12,13 +13,40 @@ interface IUserActions {
 
 const Actions: FC<IUserActions> = ({ user }) => {
    const { damageCell, placeShip, resetGame, gameStatus } = useGameStore();
+   const { damageRandomCell } = useDamageRandomCell();
    const selectedCells = useSelectedCells(gameStatus);
+
+   const [isEnemyTurn, setIsEnemyTurn] = useState<boolean>(false);
+   const [isEnemyReady, setIsEnemyReady] = useState<boolean>(true);
+
+   useEffect(() => {
+      let timer: ReturnType<typeof setTimeout>;
+
+      if (isEnemyTurn) {
+         timer = setTimeout(() => {
+            damageRandomCell();
+
+            setIsEnemyTurn(false);
+            setIsEnemyReady(true);
+         }, 3000)
+      }
+
+      return () => {
+         if (timer) clearTimeout(timer);
+      }
+   }, [isEnemyTurn, damageRandomCell]);
 
    if (!selectedCells.length) {
       return null;
    }
 
    const selectedCellsIds = selectedCells.map((cell) => cell.id);
+
+   const handleDamageCell = () => {
+      damageCell(selectedCells[0].id);
+      setIsEnemyTurn(true);
+      setIsEnemyReady(false);
+   }
 
    const renderButtons = () => {
       if (user === 'player' && gameStatus === 'placement') {
@@ -27,8 +55,15 @@ const Actions: FC<IUserActions> = ({ user }) => {
       else if (user === 'enemy' && gameStatus === 'in progress') {
          return (
             <>
-               <ActionButton text='Атаковать' onClick={() => damageCell(selectedCells[0].id)} />
-               <ActionButton text='Начать игру заново' onClick={resetGame} />
+               <ActionButton 
+                  text='Атаковать' 
+                  onClick={handleDamageCell} 
+                  disabled={!isEnemyReady}
+               />
+               <ActionButton 
+                  text='Начать игру заново' 
+                  onClick={resetGame} 
+               />
             </>
          )
       }
