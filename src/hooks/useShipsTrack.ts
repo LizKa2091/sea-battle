2be666d354c2ec71c22ad9edfe2481afa14c4cf2
lizka <lizka4231@gmail.com>
2025-12-10 23:12:07 +0300

    fix(useShipsTrack): filter enemy's ships by not destroyed state

--- src/hooks/useShipsTrack.ts
+++ src/hooks/useShipsTrack.ts
@@ -15,10 +15,10 @@ export const useShipsTrack = (gameStatus: GameStatus): Record<ShipType, number> 
 
    else {
       return {
-         single: enemyShips.filter((ship) => ship.type === 'single' && ship.state === 'ready').length,
-         duo: enemyShips.filter((ship) => ship.type === 'duo' && ship.state === 'ready').length,
-         trio: enemyShips.filter((ship) => ship.type === 'trio' && ship.state === 'ready').length,
-         quadro: enemyShips.filter((ship) => ship.type === 'quadro' && ship.state === 'ready').length
+         single: enemyShips.filter((ship) => ship.type === 'single' && ship.state !== 'destroyed').length,
+         duo: enemyShips.filter((ship) => ship.type === 'duo' && ship.state !== 'destroyed').length,
+         trio: enemyShips.filter((ship) => ship.type === 'trio' && ship.state !== 'destroyed').length,
+         quadro: enemyShips.filter((ship) => ship.type === 'quadro' && ship.state !== 'destroyed').length
       }
    }
 }
